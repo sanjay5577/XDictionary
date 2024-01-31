0a28a54c552b58450691e0b83b3e5929a4e91320
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-01-31 18:13:52 +0000

    trial

---
+++ dictionary/src/App.js
@@ -0,0 +1,56 @@
+import useState from "react"
+import './App.css';
+
+const dictionary = 
+[
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+
+function App() {
+
+  const[input , setInput]= useState('');
+  const[dictvalue ,setDictValue] =useState('');
+
+  const handleinput =(e)=>{
+
+    setInput(e.target.value)
+  }
+
+  const handlesearch=()=>{
+    const value =dictionary.find((arr)=>{
+      return arr.word.toLowerCase()===input.toLowerCase();
+
+    })
+
+    setDictValue(value.meaning);
+  }
+
+
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <div>
+        <input
+        type ="text"
+        name="inputdict"
+         value={input}
+        
+        onChange={handleinput}
+        />
+        <button onClick= {handlesearch}>Search</button>
+      </div>
+      <h4>Definition:</h4>
+      {dictvalue ? <p>{dictvalue}</p>: <p>Word not found in the dictionary</p>}
+    </div>
+
+  );
+}
+
+export default App;
